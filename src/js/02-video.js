4f1a1266d9ab48094b84943c7c050982dd2d202d
a-pinchuk <co.born2code@gmail.com>
2022-12-18 01:18:12 +0200

    finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_TIME = 'videoplayer-current-time';
+
+const ref = {
+  iframe: document.querySelector('iframe'),
+};
+
+const player = new Player(ref.iframe);
+
+player.on('timeupdate', throttle(setLocalStorage, 1000));
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem(LOCALSTORAGE_TIME)).seconds
+);
+
+function setLocalStorage(data) {
+  localStorage.setItem(LOCALSTORAGE_TIME, JSON.stringify(data));
+}
